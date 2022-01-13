1ae48a2c3679062f72c7a6d761dc741e87292270
RashmiKhanagwal <rashmi.khanagwal@gmail.com>
2022-01-13 16:08:08 +0530

    coundwords-1d.js

--- countWords-1d.js
+++ countWords-1d.js
@@ -1,31 +1,13 @@
-/* 1.c. After cleaning the text in the sentence from question number b you will get the following text. Count the number of words in this text. Don't include words with only one letter.
-
+/* 1.d. How many words were used to construct this sentence. Now, don't exclude one letter words.
 ```js
-const sentence =  `I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`
-console.log(countWords(sentence));
-31
-
+console.log(varietyOfWords(sentence))
+28
 ``` */
 
+const sentence =  `I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`
+
 function countWords(sentence){
-    let words = sentence.split(" ");
-    let count = 0;
-    for(let i=0; i< words.length; i++){
-        count++;
-    }
-    return count;
+    return sentence.trim().split(/\s+/).length;
 }
 
-const sentence =  `I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`;
-
 console.log(countWords(sentence));
-
-
-// Another solution ---- same question
-
-// const sentence =  `I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`
-
-// function countWords(sentence){
-//     return sentence.trim().split(/\s+/).length;
-//     }
-//     console.log(countWords(sentence));
